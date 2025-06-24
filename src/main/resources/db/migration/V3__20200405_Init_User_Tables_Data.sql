431dad0404e31ea7a89e67b5fab5456b2157d960
Srikar2610 <srikarpadakanti10@gmail.com>
2025-06-24 18:00:41 +0530

    Update V3__20200405_Init_User_Tables_Data.sql

--- src/main/resources/db/migration/V3__20200405_Init_User_Tables_Data.sql
+++ src/main/resources/db/migration/V3__20200405_Init_User_Tables_Data.sql
@@ -1,38 +1,21 @@
---
--- Table structure for table user
---
+INSERT INTO primary_account (id, account_balance, account_number)
+VALUES (1, 1700.00, '11223146'),
+       (2, 0.00, '11223150');
 
-DROP TABLE IF EXISTS user;
-CREATE TABLE user
-(
-    user_id            bigint(20) NOT NULL AUTO_INCREMENT,
-    email              varchar(255) NOT NULL,
-    enabled            bit(1)       NOT NULL,
-    first_name         varchar(255) DEFAULT NULL,
-    last_name          varchar(255) DEFAULT NULL,
-    password           varchar(255) DEFAULT NULL,
-    phone              varchar(255) DEFAULT NULL,
-    username           varchar(255) DEFAULT NULL,
-    primary_account_id bigint(20) DEFAULT NULL,
-    savings_account_id bigint(20) DEFAULT NULL,
-    PRIMARY KEY (user_id),
-    UNIQUE KEY UK_ob8kqyqqgmefl0aco34akdtpe (email),
-    KEY                FKbj0uoj9i40dory8w4t5ojyb9n(primary_account_id),
-    KEY                FKihums7d3g5cv9ehminfs1539e(savings_account_id),
-    CONSTRAINT FKbj0uoj9i40dory8w4t5ojyb9n FOREIGN KEY (primary_account_id) REFERENCES primary_account (id),
-    CONSTRAINT FKihums7d3g5cv9ehminfs1539e FOREIGN KEY (savings_account_id) REFERENCES savings_account (id)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+INSERT INTO savings_account (id, account_balance, account_number)
+VALUES (1, 2500.00, '9876543210'),
+       (2, 500.00, '9876543211');
 
---
--- Dumping data for table user
---
+INSERT INTO role (role_id, name) VALUES (1, 'ROLE_USER');
+INSERT INTO role (role_id, name) VALUES (2, 'ROLE_ADMIN');
 
-LOCK TABLES USER WRITE;
-INSERT INTO user
-VALUES (1, 'uzumaki_naruto@konohagakure.co.jp', '', 'Uzumaki', 'Naruto',
-        '$2a$12$DWCryEwHwbTYCegib92tk.VST.GG1i2WAqfaSwyMdxX0cl0eBeSve', '5551112345', 'User', 1, 1),
-       (2, 'uchiha_sasuke@konohagakure.co.jp', '', 'Uchiha', 'Sasuke',
+INSERT INTO user (user_id, email, enabled, first_name, last_name, password, phone, username, primary_account_id, savings_account_id)
+VALUES (1, 'uzumaki_naruto@konohagakure.co.jp', TRUE, 'Uzumaki', 'Naruto',
+        '$2a$12$DWCryEwHwbTYCegib92tk.VST.GG1i2WAqfaSwyMdxX0cl0eBeSve', '5551112345', 'UzumakiNaruto', 1, 1),
+       (2, 'uchiha_sasuke@konohagakure.co.jp', TRUE, 'Uchiha', 'Sasuke',
         '$2a$12$hZR7pcSf0JU/OTXR3TOyuu8r6C6n.JZE8Ei47E4LZs1t0Aq1AO6oC',
-        '1111111111', 'Admin', 2, 2);
-UNLOCK TABLES;
+        '1111111111', 'UchihaSasuke', 2, 2);
 
+INSERT INTO user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO user_role (user_id, role_id) VALUES (2, 2);
+INSERT INTO user_role (user_id, role_id) VALUES (2, 1);
